c70585674b0c8c29ab4ab31ec5a63641ff317309
Jose Zurita <jose.zurita@epn.edu.ec>
2017-05-09 16:52:34 -0500

    Compatibilidad con ES6

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,3 +5,23 @@ Comentario de varias líneas
  */
 
 console.log("Hello World");
+
+var jose = {
+    nombre:"José",
+    apellido: "Zurita",
+    fechaNacimiento: new Date(),
+    imprimir:function() {
+        console.log("nombre completo: " + this.nombre + " " + this.apellido )
+    },
+    ano:function() {
+        return this.fechaNacimiento.getFullYear();
+    },
+    diferenciaEdad:function(edadAComparar){
+        return Math.abs(this.fechaNacimiento.getFullYear()-edadAComparar);
+    }
+};
+
+
+jose.imprimir();
+console.log(jose.ano());
+console.log(jose.diferenciaEdad(1997));
