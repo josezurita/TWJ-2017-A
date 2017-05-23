8de3642d362f769e6b3ef356891d518dfed29747
Jose Zurita <jose.zurita@epn.edu.ec>
2017-05-23 15:25:28 -0500

    Igualado clase 17 de mayo

--- Angular/A-Web/src/app/app.component.ts
+++ Angular/A-Web/src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'twj-raiz',
+  templateUrl: "./app.component.html" ,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Hola amigos';
-  nombre:string = "Adrian";
-  apellido:string = "Eguez";
+  usuario:UsuarioInt = {
+    nombre:"Adrian",
+    apellido:"Eguez"
+  };
+
+  url:string="https://angular.io";
+  src:string="https://angular.io/resources/images/logos/angular/angular.png"
+
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+    console.log(
+      this.saludar(this.usuario.nombre,
+      this.usuario.apellido
+    ));
+  }
+
+  holaMundo(){
+    console.log("Hola mundo");
+  }
+  saludar(nombre:string,
+          apellido?:string):string{
+    return `Hola ${nombre} ${apellido}`;
+  }
+
 }
+
+
+
+
+interface UsuarioInt{
+  nombre:string,
+  apellido:string
+}
+
