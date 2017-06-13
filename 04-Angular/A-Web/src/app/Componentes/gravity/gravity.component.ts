3b0e7be8a942b1a2b3b737dbe64cb15bf46de616
Jose Zurita <jose.zurita@epn.edu.ec>
2017-06-13 15:59:52 -0500

    Creados componentes StarWars

---
+++ 04-Angular/A-Web/src/app/Componentes/gravity/gravity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gravity',
+  templateUrl: './gravity.component.html',
+  styleUrls: ['./gravity.component.css']
+})
+export class GravityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- 04-Angular/A-Web/src/app/app.module.ts
+++ 04-Angular/A-Web/src/app/app.module.ts
@@ -7,6 +7,10 @@ import {NgModule} from "@angular/core";
 import { EstiloComponent } from './estilo/estilo.component';
 import {InicioComponent} from "./Componentes/inicio/inicio.component";
 import { PlanetaStarWarsComponent } from './Componentes/planeta-star-wars/planeta-star-wars.component';
+import { OrbitalComponent } from './Componentes/orbital/orbital.component';
+import { RotationComponent } from './Componentes/rotation/rotation.component';
+import { TerrainComponent } from './Componentes/terrain/terrain.component';
+import { GravityComponent } from './Componentes/gravity/gravity.component';
 
 
 @NgModule({
@@ -14,7 +18,11 @@ import { PlanetaStarWarsComponent } from './Componentes/planeta-star-wars/planet
     AppComponent,
     EstiloComponent,
     InicioComponent,
-    PlanetaStarWarsComponent
+    PlanetaStarWarsComponent,
+    OrbitalComponent,
+    RotationComponent,
+    TerrainComponent,
+    GravityComponent
   ],
   imports: [
     BrowserModule,
