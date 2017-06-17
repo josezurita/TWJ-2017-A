609dc2f128b716fa6e9a7c271c0fde3ef39cc419
josezurita <jose.zurita@epn.edu.ec>
2017-06-17 10:05:24 -0500

    Añadido carrusel, no funciona

--- Examen/twj-examen-api-zurita-jose/src/app/app.module.ts
+++ Examen/twj-examen-api-zurita-jose/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
 import {HttpModule} from "@angular/http";
 import { InicioComponent } from './inicio/inicio.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieComponent,
-    InicioComponent
+    InicioComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
