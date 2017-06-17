4c16322bae8e206a99b0024ebcd267a99d27eaa5
Jose Zurita <jose.zurita@epn.edu.ec>
2017-06-16 23:36:24 -0500

    Componente movido a la carpeta components

--- Examen/twj-examen-zurita-jose/src/app/app.module.ts
+++ Examen/twj-examen-zurita-jose/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { PasteleriaComponent } from './pasteleria/pasteleria.component';
+import { PasteleriaComponent } from '../components/pasteleria/pasteleria.component';
 
 @NgModule({
   declarations: [
