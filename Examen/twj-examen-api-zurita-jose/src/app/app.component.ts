f1097b74828c8081e00b093517bb3a75fe5907b6
José Zurita <jose.zurita@epn.edu.ec>
2017-06-17 04:43:39 -0500

    Creado componente, interfaz y método para obtener los datos

--- Examen/twj-examen-api-zurita-jose/src/app/app.component.ts
+++ Examen/twj-examen-api-zurita-jose/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieInterface} from "../classes/MovieInterface";
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  movieList: MovieInterface[];
+
+  constructor(private _http:Http) { }
+
+  ngOnInit() {
+    this._http.get("http://netflixroulette.net/api/api.php?director=Quentin%20Tarantino").subscribe(respuesta=>{
+      let rJson = respuesta.json();
+      console.log("respuesta json:", rJson);
+      this.movieList=rJson;
+
+    }, error=>{
+      console.log("error: ", error);
+    });
+  }
 }
---
+++ Examen/twj-examen-api-zurita-jose/src/classes/MovieInterface.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Familia on 17/06/2017.
+ */
+export interface MovieInterface {
+  unit?: number,
+  show_id?: number,
+  show_title?: string,
+  release_year?: number,
+  rating?: string,
+  category?: string,
+  show_cast?: string,
+  director?: string,
+  summary?: string,
+  poster?: string,
+  mediatype?: number,
+  runtime?: string
+}
