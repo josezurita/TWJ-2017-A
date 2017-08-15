33af21ecded9243ee5d75daf03da4577f1e32305
josezurita <jose.zurita@epn.edu.ec>
2017-08-15 18:25:40 -0500

    Agregada logica en pagina de Mi lista

--- Examen2do/twj-examen-api-zurita-jose/src/app/lista/lista.component.ts
+++ Examen2do/twj-examen-api-zurita-jose/src/app/lista/lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieInterface } from "../../classes/MovieInterface";
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-lista',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista.component.css']
 })
 export class ListaComponent implements OnInit {
-
-  constructor() { }
+  movieList: MovieInterface[];
+  constructor(private _http:Http) { }
 
   ngOnInit() {
+    console.log("entra a on init");
+    this._http.get("https://javascript2017a.mybluemix.net/pelicula").subscribe(response=>{
+      let jsonResponse = response.json();
+      console.log(jsonResponse);
+      this.movieList=jsonResponse;
+      console.log(this.movieList);
+    },error2 => {
+      console.log(error2);
+    })
   }
 
 }
